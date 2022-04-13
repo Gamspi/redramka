65cb502fe59f35a351d9c9f2b407c08fd02ff8b4
Gamspi <niko.tatt007@gmail.com>
2022-04-14 01:19:28 +0700

    исправление карточки info

--- src/core/components/info/Info.jsx
+++ src/core/components/info/Info.jsx
@@ -13,6 +13,16 @@ const Info = ({
                   setIsInfo,
     setIsKonfem
               }) => {
+    const handlerClose = (event) => {
+      if (event.target.classList.contains("info__button-close")||!event.target.closest(".info__body")){
+          setIsInfo(false)
+
+      }else if (event.target.classList.contains("info__button")){
+          setIsKonfem(true)
+      }
+
+
+    }
 
     return (
         <CSSTransition
@@ -22,7 +32,7 @@ const Info = ({
             unmountOnExit
             mountOnEnter
         >
-            <div className="info">
+            <div className="info" onClick={(event)=>handlerClose(event)}>
                 <div className="info__body">
                     <img src={logo} alt={name}/>
                     <h2>Name: {name}</h2>
@@ -31,8 +41,8 @@ const Info = ({
                     <p>Slogan: {slogan}</p>
                     <p>Head quaters: {head_quaters}</p>
                     <p>Website: <a href={website}>{website}</a></p>
-                    <button className="info__button" onClick={()=>setIsKonfem(true)}>Удалить</button>
-                    <button className="info__button-close" onClick={()=>setIsInfo(false)}>X</button>
+                    <button className="info__button" >Удалить</button>
+                    <button className="info__button-close" >X</button>
                 </div>
             </div>
         </CSSTransition>
