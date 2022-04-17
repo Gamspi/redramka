b2f37c5e5a0559b3522b37cc2d264527c9cd54dd
Gamspi <niko.tatt007@gmail.com>
2022-04-17 09:16:35 +0700

    добавление стилей

--- src/core/components/info/Info.tsx
+++ src/core/components/info/Info.tsx
@@ -30,12 +30,12 @@ const Info = ({
             <div className="info" onClick={(event) => handlerClose(event)}>
                 <div className="info__body">
                     <img src={card.logo} alt={card.name}/>
-                    <h2>Name: {card.name}</h2>
-                    <p>Country: {card.country}</p>
-                    <p>Established: {card.established}</p>
-                    <p>Slogan: {card.slogan}</p>
-                    <p>Head quaters: {card.head_quaters}</p>
-                    <p>Website: <a href={card.website}>{card.website}</a></p>
+                    <h2>Name: <span>{card.name}</span> </h2>
+                    <p>Country: <span>{card.country}</span></p>
+                    <p>Established: <span>{card.established}</span></p>
+                    <p>Slogan: <span>{card.slogan}</span></p>
+                    <p>Head quaters: <span>{card.head_quaters}</span></p>
+                    <p>Website: <span><a href={card.website}>{card.website}</a></span></p>
                     <button className="info__button">Удалить</button>
                     <button className="info__button-close">&#10008;</button>
                 </div>
